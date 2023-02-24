01cab0c00ecffb56a59da395e6ef8194f879c98c
Victor Tavares <victor.tavares@globant.com>
2023-02-24 12:18:45 -0600

    feat: Adding eslint plugin and axe-core to autoaudit accessibility best practices

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axe from '@axe-core/react';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
+
 root.render(
   <React.StrictMode>
     <App />
