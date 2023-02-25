93d00fb754626f138e53af3891f8eadf093d6506
Victor Tavares <victor.tavares@globant.com>
2023-02-24 18:38:23 -0600

    feat: removing unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App(): JSX.Element {
   return (
-    <div className="App" id="">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
